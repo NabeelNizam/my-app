65df08e1e5db83457e9de1887335e2ae42a2c02d
Nabeel Nizam <abee237@gmail.com>
2026-02-19 13:53:33 +0700

    Praktikum 2

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Halaman About</h1>
+      <p>Nama: Nabeel Nizam</p>
+      <p>NIM: 2341720155</p>
+      <p>Program Studi : D4-Teknik Informatika</p>
+      <a href="/">Kembali ke Home</a>
+    </div>
+  )
+}
