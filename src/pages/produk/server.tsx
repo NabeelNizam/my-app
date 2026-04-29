11405474ad3d38ae61b778c90c581db747e9f6db
Nabeel Nizam <abee237@gmail.com>
2026-04-29 15:02:42 +0700

    modify

--- src/pages/produk/server.tsx
+++ src/pages/produk/server.tsx
@@ -1,25 +1,32 @@
 import TampilanProduk from "../../views/produk";
 import { ProductType } from "../../types/Product.type";
-import { retrieveProducts } from "@/utils/db/servicefirebase";
 
-const halamanProdukServer = (props: { products: ProductType[] }) => {
-    const { products } = props;
-    return (
-        <div>
-            <h1>Halaman Produk Server</h1>
-            <TampilanProduk products={products} isLoading={false} detailBasePath="/produk/ssr" />
-        </div>
-    )
-}
+type Props = {
+  products: ProductType[];
+};
 
-export default halamanProdukServer;
+const HalamanProdukServer = ({ products }: Props) => {
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduk
+        products={products}
+        isLoading={false}
+        detailBasePath="/produk/server"
+      />
+    </div>
+  );
+};
+
+export default HalamanProdukServer;
 
 export async function getServerSideProps() {
-    const products = await retrieveProducts("products");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/produk`);
+  const response = await res.json();
 
-    return {
-        props: {
-            products
-        }
-    }
+  return {
+    props: {
+      products: response.data || [],
+    },
+  };
 }
